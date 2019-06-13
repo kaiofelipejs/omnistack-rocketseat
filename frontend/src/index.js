e4d9935a30f98e85e1ae9efc6fa0d8b501e47fa9
Kaio <kaiofelipe.js@gmail.com>
2019-06-13 00:07:19 -0300

    created frontend (feed and upload images) in realtime

---
+++ frontend/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // integração do react com o browser
+import App from './App'; 
+import './global.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
