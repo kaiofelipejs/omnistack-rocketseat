758bb6fca5ec0df15e94a8912bdb9710d6c21803
Kaio <kaiofelipe.js@gmail.com>
2019-06-17 21:37:14 -0300

    add styled components in new post page

--- frontend/src/pages/New.js
+++ frontend/src/pages/New.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 
-import './New.css';
+import { NewPost } from './NewStyles';
 
 class New extends Component {
     state = {
@@ -38,7 +38,7 @@ class New extends Component {
 
     render() {
         return (
-            <form id="new-post" onSubmit={this.handleSubmit}>
+            <NewPost onSubmit={this.handleSubmit}>
                 <input type="file" onChange={this.handleImageChange} />
 
                 <input 
@@ -74,7 +74,7 @@ class New extends Component {
                 />
 
                 <button type="submit">Enviar</button>
-            </form>
+            </NewPost>
         );
     }
 }
